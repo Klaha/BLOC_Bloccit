23f2aa3cf858da0a3ab712daac36b1c85c91b5fb
Klaha <klaha.77@gmail.com>
2015-03-12 12:31:52 -0430

    Authentication Assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+  skip_before_action :flash_attack, only: [:index, :new]
+
   def index
     @posts = Post.all
   end
@@ -39,4 +42,5 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+
 end
